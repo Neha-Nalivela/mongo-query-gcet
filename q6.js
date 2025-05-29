4187f4c053cc31195e19204028b1926ea8d3ad24
Neha-Nalivela <nehareddynalivela@gmail.com>
2025-05-29 19:54:52 +0530

    update

---
+++ q6.js
@@ -0,0 +1,12 @@
+db.employees.updateOne({ email: "john@gmail.com" }, { $set: { email: "john@outlook.com" } });
+db.employees.find().sort({ salary: -1 }).limit(3); 
+db.employees.updateMany({}, { $inc: { salary: 1000 } });
+db.employees.insertOne({
+  name: "Anjali",
+  email: "anjali@gmail.com",
+  department: "Admin",
+  salary: 1456,
+  location: ["FL", "OH"],
+  date: Date(),
+});
+db.employees.deleteOne({ department: "Admin" });
