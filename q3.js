4d6bf2d42ae296a86237c79f34d40ce0293cce99
Neha-Nalivela <nehareddynalivela@gmail.com>
2025-05-29 18:47:10 +0530

    update

---
+++ q3.js
@@ -0,0 +1,21 @@
+//use products
+db.products.insertMany([
+    {
+        name: "Apple",
+        description: "fruits", 
+        imgUrl: "https://picsum.photos/200/300",
+        price: 30
+    },
+    {
+        name: "Mango",
+        description: "fruits", 
+        imgUrl: "https://picsum.photos/200/300",
+        price: 50
+    },
+    {
+        name: "Tomato",
+        description: "Vegetables", 
+        imgUrl: "https://picsum.photos/200/300",
+        price: 10
+    },
+])
